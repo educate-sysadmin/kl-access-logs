34df4aa85437298abbde961217c6206da1630a8b
Brent C <educate-sysadmin@ucl.ac.uk>
2018-10-24 11:07:51 +0100

    Return value check for hook klal_track (to allow cancel)

--- kl-access-logs.php
+++ kl-access-logs.php
@@ -272,7 +272,12 @@ function klal_track () {
 	    }
 	    if (!$track) return;
 	    
-		$klal_filter = apply_filters('klal_track', array('context'=>'klal_track'));	    
+	    // apply hook filter
+		$klal_filter = apply_filters('klal_track', array('context'=>'klal_track'));
+		// check return value, allowing cancel
+		if (!$klal_filter) {			
+			return;
+		}
 				
 	    // save as very modified Combined CLF 
 	    // e.g. 127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"	    
